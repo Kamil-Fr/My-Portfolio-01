eb9eef0130ec34d79fb9ed6996f40ba1443ed62e
Kamil-Fr <kamil.korzeniowski.fr@gmail.com>
2025-01-03 19:58:13 +0100

    Add initial files.

---
+++ src/components/Hero.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Hero() {
+  return (
+    <div className="relative overflow-hidden min-h-[550px] sm:min-h-[660px] flex flex-col items-center">
+      <div className="md:h-[550px] h-[550[\px] w-[450px] bg-gradient-to-r absolute from-[#304075] via-[#1420c5] to-[#617cc5] transform rotate-45 z-0 right-2 top-28 rounded-3xl"></div>
+    </div>
+  );
+}
