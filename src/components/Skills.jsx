da60d91ac29995b335e3b08805dc54c767bd4c9e
Kamil-Fr <kamil.korzeniowski.fr@gmail.com>
2025-01-06 23:12:05 +0100

    Add Skills section part 1.

---
+++ src/components/Skills.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import imghero from "../assets/images/imghero.png";
+import { description } from "commander";
+
+const skillsData = [
+  {
+    id: 1,
+    image: React,
+    title: "React JS",
+    description: "Lorem ipsum dolor sit amet.",
+  },
+  {
+    id: 1,
+    image: React,
+    title: "React JS",
+    description: "Lorem ipsum dolor sit amet.",
+  },
+  {
+    id: 1,
+    image: React,
+    title: "React JS",
+    description: "Lorem ipsum dolor sit amet.",
+  },
+  {
+    id: 1,
+    image: React,
+    title: "React JS",
+    description: "Lorem ipsum dolor sit amet.",
+  },
+];
+
+export default function Skills() {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem,
+      laboriosam dignissimos? Odio iusto praesentium, inventore dicta dolores
+      odit, architecto ex tenetur impedit facere, sapiente accusantium porro
+      fugiat possimus repellendus dolor?
+    </div>
+  );
+}
