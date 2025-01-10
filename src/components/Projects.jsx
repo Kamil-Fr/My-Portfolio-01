96e232b1d2c5e459aded50af5e11830c51d23fe6
Kamil-Fr <kamil.korzeniowski.fr@gmail.com>
2025-01-10 22:43:53 +0100

    Add Projects section.

---
+++ src/components/Projects.jsx
@@ -0,0 +1,98 @@
+import React from "react";
+import project1 from "../assets/images/project1.png";
+import project2 from "../assets/images/project2.png";
+import project3 from "../assets/images/project3.png";
+import project4 from "../assets/images/project4.png";
+
+const ProjectCard = ({ image, title, description, link }) => {
+  return (
+    <article className="relative max-w-sm bg-gray-800 rounded overflow-hidden shadow-lg group">
+      <div className="absolute z-0 w-40 h-40 sm:w-60 sm:h-60 bg-[#3c7df5] rounded-full blur-3xl opacity-50 -top-5 left-10"></div>
+      <div className="relative z-10">
+        <figure className="relative">
+          <img
+            src={image}
+            alt={title}
+            className="w-full h-48 object-cover transition-transform duration-300 group-hover:scale-110"
+          />
+          <a
+            href={link}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="absolute h-[202px] inset-0 flex items-center justify-center bg-blue-500 bg-opacity-50 opacity-0
+            group-hover:opacity-100 transition-opacity duration-300"
+          >
+            <button className="bg-white font-medium text-black py-2 px-4 rounded-3xl shadow hover:text-white hover:bg-[#2879d5]">
+              Live Preview
+            </button>
+          </a>
+        </figure>
+        <div className="px-6 py-4">
+          <header>
+            <h3 className="text-white font-bold text-xl mb-2">{title}</h3>
+          </header>
+          <p className="text-gray-200 text-base">{description}</p>
+        </div>
+      </div>
+    </article>
+  );
+};
+
+export default function Projects() {
+  const listProjects = [
+    {
+      image: project1,
+      title: "Loan",
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      link: "https://github.com/Kamil-Fr/Loans_landing_page",
+    },
+    {
+      image: project2,
+      title: "Give it to good hands",
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      link: "https://kamil-fr.github.io/give_it_to_good_hands/",
+    },
+    {
+      image: project3,
+      title: "Paris",
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      link: "https://kamil-fr.github.io/paris/",
+    },
+    {
+      image: project4,
+      title: "Vidoo",
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      link: "https://github.com/Kamil-Fr/Vidoo",
+    },
+  ];
+  return (
+    <main className="p-4">
+      <section data-aos="fade-up" data-aos-delay="300">
+        <header className="text-center">
+          <h1 className="text-3xl text-white sm:text-4xl font-bold mb-6">
+            My <span className="text-blue-400">Projects</span>
+          </h1>
+          <p className="text-gray-400 mt-2 sm:mt-4 text-sm sm:text-base">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est vero
+            sint debitis distinctio dolorum dicta molestias quam.
+          </p>
+        </header>
+      </section>
+      <section
+        data-aos="fade-up"
+        data-aos-delay="500"
+        className="flex flex-wrap gap-4 justify-center mt-6"
+      >
+        {listProjects.map((project, index) => (
+          <ProjectCard
+            key={index}
+            image={project.image}
+            title={project.title}
+            description={project.description}
+            link={project.link}
+          />
+        ))}
+      </section>
+    </main>
+  );
+}
