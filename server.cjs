3c84df18d4ef1fba46438ee80d5c9a0f6975d341
Kamil-Fr <kamil.korzeniowski.fr@gmail.com>
2025-01-17 14:24:02 +0100

    Change.

--- server.cjs
+++ server.cjs
@@ -13,47 +13,6 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "dist"))); // Obsługa plików statycznych
 
-// // Endpoint do obsługi wysyłania e-maili
-// app.post("/send-email", async (req, res) => {
-//   const { name, email, message } = req.body;
-
-//   // Walidacja danych wejściowych
-//   if (!name || !email || !message) {
-//     return res.status(400).json({ error: "All fields are required." });
-//   }
-
-//   try {
-//     // Konfiguracja transportera nodemailer
-//     const transporter = nodemailer.createTransport({
-//       host: "smtp.gmail.com",
-//       port: 587,
-//       secure: false, // Użyj TLS
-//       auth: {
-//         user: process.env.EMAIL_USER, // Twój e-mail
-//         pass: process.env.EMAIL_PASS, // Twoje hasło aplikacji
-//       },
-//     });
-
-//     // Opcje e-maila
-//     const mailOptions = {
-//       from: `"${name}" <${email}>`,
-//       to: process.env.EMAIL_TO, // Odbiorca
-//       subject: `Nowa wiadomość od ${name}`,
-//       text: message,
-//       html: `<p><strong>Imię:</strong> ${name}</p>
-//              <p><strong>Email:</strong> ${email}</p>
-//              <p><strong>Wiadomość:</strong></p>
-//              <p>${message}</p>`,
-//     };
-
-//     // Wysyłanie e-maila
-//     await transporter.sendMail(mailOptions);
-//     res.status(200).json({ message: "Email sent successfully!" });
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).json({ error: "Failed to send email. Try again later." });
-//   }
-// });
 app.post("/send-email", async (req, res) => {
   console.log("Otrzymano żądanie POST na /send-email");
   console.log("Body żądania:", req.body);
@@ -105,7 +64,7 @@ app.get("*", (req, res) => {
 
 // Uruchamianie serwera
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on ${PORT}`);
 });
 
 
