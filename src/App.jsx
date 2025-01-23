f1b1aaba6b095929c1fdf9535e94eb56dfe8a0d0
Kamil-Fr <kamil.korzeniowski.fr@gmail.com>
2025-01-23 18:24:31 +0100

    Change.

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Skills from "./components/Skills";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
+
 import AOS from "aos";
 import "aos/dist/aos.css";
 
@@ -21,7 +22,7 @@ function App() {
       <Skills />
       <Projects />
       <Contact />
-      <Footer />
+          <Footer />
     </main>
   );
 }
